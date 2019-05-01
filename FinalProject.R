26084ad680511de5ce0ea46dfa70f5129bbe276b
jamesponwith <jponwith@sandiego.edu>
2019-05-01 16:13:37 -0700

    some more stuff done

--- FinalProject.R
+++ FinalProject.R
@@ -8,12 +8,26 @@
 
 # Importing the dataset
 dataset = read.csv('master.csv')
+
+# Create Dataframe to encode Country data
 countries_col = dataset[1]
 uni = unique(countries_col)
 uni$ID <- seq.int(nrow(uni))
 
-age_col = levels(dataset$age)
-age_col
+# Create Dataframe to encode Country data
+age_col = dataset[4]
+uni_age = unique(age_col)
+uni_age$ID <- seq.int(nrow(uni_age))
+
+# Create Dataframe to encode Sex data
+sex_col = dataset[3]
+uni_sex = unique(sex_col)
+uni_sex$ID <- seq.int(nrow(uni_sex))
+
+# Create Dataframe to encode Generation data
+gen_col = dataset[12]
+uni_gen = unique(gen_col)
+uni_gen$ID <- seq.int(nrow(uni_gen))
 
 
 # Splitting the dataset into the Training set and Test set
@@ -23,10 +37,27 @@ age_col
 #       Convert Categorical Data to Numerical Data (age ranges)
 #       
 
-# Encoding categorical data
+# Encoding Categorical Data as Numerical Data
+
+# Encode Country data (1-101)
 dataset$country = factor(dataset$country,
                        levels = uni$country,
                        labels = uni$ID)
+
+# Encode age ranges (1-6)
+dataset$age = factor(dataset$age,
+                         levels = uni_age$age,
+                         labels = uni_age$ID)
+
+dataset$sex = factor(dataset$sex,
+                       levels = uni_sex$sex,
+                       labels = uni_sex$ID)
+
+dataset$generation = factor(dataset$generation,
+                            levels = uni_gen$generation,
+                            labels = uni_gen$ID)
+
+dataset <- subset(dataset, select = -c(8, 9))
 
 library(caTools)
 set.seed(123)
@@ -39,10 +70,17 @@ test_set = subset(dataset, split == FALSE)
 
 # STUFF BELOW DOESNT WORK 
 
+#####################################################################
+#####################################################################
+### DATA IS CLEAN BUT THE VARIABLES ARE WEIRD WHEN REGRESSOR IS BUILT
+#####################################################################
+#####################################################################
 
 # Fitting Multiple Linear Regression to the Training Set
-regressor = lm(formula = suicides_no ~ ., 
-               data = training_set) # Express independent variable as a linear combination of all dependent variables
+regressor = lm(formula = suicides_no ~.,
+               data = training_set)
+#regressor = lm(formula = suicides_no ~ country + year + sex + age + population + suicides.100k.pop + gdp_for_year.... + gdp_per_capita...., 
+#               data = training_set) # Express independent variable as a linear combination of all dependent variables
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
